5dae0ddba77224adfa423b4b1bbe7b1811da9263
ivan-kramarenko <kramarenko-ivan@proton.me>
2024-11-13 12:21:57 +0200

    Error fixing #4

--- BookShelfApp.py
+++ BookShelfApp.py
@@ -7,7 +7,7 @@ from controllers.data_controller import DataController
 
 # Основной класс для приложения
 class BookShelfApp:
-    def __init__(self, root):
+    def __init__(self):
         self.root = root
         self.root.title("Книжкова полиця")
 
@@ -17,7 +17,7 @@ class BookShelfApp:
 
         # Инициализация контроллеров с доступом к спискам
         self.book_controller = BookController(self, self.book_list)
-        self.author_controller = AuthorController(self, self.author_list)
+        self.author_controller = AuthorController(self, self.author_list, self.book_list)
         self.data_controller = DataController(self, self.book_list, self.author_list)
 
         # Создаем меню
